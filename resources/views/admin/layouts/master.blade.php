ca25f6e28ca64dc2edca6a4986437e2efac306e7
Utsab Dey <utsabdey83@gmail.com>
2022-07-21 11:04:12 +0600

    Admin Setup-3

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -2,22 +2,25 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title') - SB Admin</title>
-
-    {{-- DataTable --}}
-    <link rel="stylesheet" href="{{ asset('backend/datatable.css') }}">
+    <title>@yield('title') - BlogSite</title>
 
     {{-- Main CSS --}}
     <link rel="stylesheet" href="{{ asset('backend/css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('backend/css/datatable.css') }}">
+
+    {{-- Font Awesome --}}
+    <script src="{{ asset('backend/fontawesome/v6.1.0/all.js') }}"></script>
+
+    {{-- Toastr CSS --}}
+    <link rel="stylesheet" href="{{ asset('backend/toastr/toastr.css') }}">
 </head>
 
 <body class="sb-nav-fixed">
-
     @include('admin.layouts.navbar')
 
     <div id="layoutSidenav">
@@ -27,20 +30,57 @@
             <main>
                 @yield('content')
             </main>
+            @include('admin.layouts.footer')
         </div>
-        @include('admin.layouts.footer')
     </div>
 
-    {{-- Font-Awesome --}}
-    <script src="{{ asset('backend/fontawesome/v6.1.0/all.js') }}"></script>
-    
+    {{-- jQuery --}}
+
+    {{-- Bootstrap JS --}}
+    <script src="{{ asset('backend/js/bootstrap.bundle.min.js') }}"></script>
+
     {{-- Main JS --}}
     <script src="{{ asset('backend/js/scripts.js') }}"></script>
-    <script src="{{ asset('backend/js/bootstrap.bundle.min.js') }}"></script>
+
+    <script src="{{ asset('backend/js/chart.min.js') }}"></script>
     <script src="{{ asset('backend/js/chart-area-demo.js') }}"></script>
     <script src="{{ asset('backend/js/chart-bar-demo.js') }}"></script>
     <script src="{{ asset('backend/js/simple-datatables.js') }}"></script>
     <script src="{{ asset('backend/js/datatables-simple-demo.js') }}"></script>
+
+    {{-- Toastr JS --}}
+    <script src="{{ asset('backend/toastr/toastr.min.js') }}"></script>
+
+    <script>
+        @if (Session::has('success'))
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true
+            }
+            toastr.success("{{ session('success') }}");
+        @endif
+        @if (Session::has('error'))
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true
+            }
+            toastr.error("{{ session('error') }}");
+        @endif
+        @if (Session::has('info'))
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true
+            }
+            toastr.info("{{ session('info') }}");
+        @endif
+        @if (Session::has('warning'))
+            toastr.options = {
+                "closeButton": true,
+                "progressBar": true
+            }
+            toastr.warning("{{ session('warning') }}");
+        @endif
+    </script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+// Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
 Route::group([
